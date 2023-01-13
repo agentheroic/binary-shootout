e394fdf92e9c9e853fe579321a5a4fd59030c127
amaanmohdsyed2011 <96779802+amaanmohdsyed2011@users.noreply.github.com>
2023-01-14 02:48:12 +0530

    Animation Added to Enemy

--- 2D-Shooter-Vinhack-2023/Assets/Scripts/EnemyPatrol.cs
+++ 2D-Shooter-Vinhack-2023/Assets/Scripts/EnemyPatrol.cs
@@ -7,15 +7,24 @@ public class EnemyPatrol : MonoBehaviour
     public Transform[] patrolPoints;
     private int currentPoint;
 
-    public float movespeed, waitAtpoints;
+    public float moveSpeed, waitAtPoints;
     private float waitCounter;
 
-    public float jumpforce;
+    public float jumpForce;
+
     public Rigidbody2D theRB;
+    public Animator anim;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        waitCounter = waitAtpoints;
+        waitCounter = waitAtPoints;
+
+        foreach(Transform pPoint in patrolPoints)
+        {
+            pPoint.SetParent(null);
+        }
     }
 
     // Update is called once per frame
@@ -23,16 +32,38 @@ public class EnemyPatrol : MonoBehaviour
     {
         if(Mathf.Abs(transform.position.x - patrolPoints[currentPoint].position.x) > .2f)
         {
-            if(transform.position.x < patrolPoints[currentPoint].position.x){
-                theRB.velocity = new Vector2(movespeed, theRB.velocity.y);
-
+            if(transform.position.x < patrolPoints[currentPoint].position.x)
+            {
+                theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            } else
+            {
+                theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+                transform.localScale = Vector3.one;
             }
-             else
+
+            if(transform.position.y < patrolPoints[currentPoint].position.y -.5f && theRB.velocity.y < .1f )
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+        } else
         {
-            theRB.velocity = new Vector2(-movespeed,theRB.velocity.y);
+            theRB.velocity = new Vector2(0f, theRB.velocity.y);
+
+            waitCounter -= Time.deltaTime;
+            if(waitCounter <= 0)
+            {
+                waitCounter = waitAtPoints;
+
+                currentPoint++;
+
+                if(currentPoint >= patrolPoints.Length)
+                {
+                    currentPoint = 0;
+                }
+            }
         }
-            
-        }
-       
+
+        anim.SetFloat("speed",Mathf.Abs(theRB.velocity.x));
     }
 }
